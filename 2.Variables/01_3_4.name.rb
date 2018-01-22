4afed1b9b6ac1afbc2d7fb4232095bb367dacdb2
tizentos <tizentos@gmail.com>
2018-01-22 13:37:57 +0100

    Chapter 2

---
+++ 2.Variables/01_3_4.name.rb
@@ -0,0 +1,8 @@
+puts "Enter you first name"
+first_name=gets.chomp
+puts "Enter you last name"
+last_name=gets.chomp
+10.times {puts "Hello #{first_name}!"}
+
+puts "Your full name is #{first_name} #{last_name}"
+gets.chomp
